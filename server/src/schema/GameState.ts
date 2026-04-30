4af0527e52fb1413fa5f55a3ddb7d1c88a6ab590
civax <LeanEntropy@users.noreply.github.com>
2026-04-30 16:28:05 +0300

    feat: define Colyseus schema for multiplayer game state

--- server/src/schema/GameState.ts
+++ server/src/schema/GameState.ts
@@ -1,20 +1,31 @@
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, ArraySchema, type } from "@colyseus/schema";
 
-export class PlayerSchema extends Schema {
-  @type("uint8") slotId: number = 0;
-  @type("float32") x: number = 0;
-  @type("float32") y: number = 0;
-  @type("float32") heading: number = 0;
+export class FactionSchema extends Schema {
+  @type("uint8") id: number = 0;
+  @type("number") territoryPct: number = 0;
   @type("boolean") alive: boolean = true;
-  @type("float32") respawnTimer: number = 0;
-  @type("float32") invulnTimer: number = 0;
-  @type("uint16") killCount: number = 0;
-  @type("uint16") territoryCount: number = 0;
+  @type("boolean") endangered: boolean = false;
+}
+
+export class CharacterSchema extends Schema {
+  @type("uint8") id: number = 0;
+  @type("uint8") factionId: number = 0;
   @type("string") name: string = "";
-  @type("uint32") color: number = 0;
+  @type("boolean") isHuman: boolean = false;
+  @type("number") posX: number = 0;
+  @type("number") posZ: number = 0;
+  @type("number") dirX: number = 0;
+  @type("number") dirZ: number = 1;
+  @type("boolean") alive: boolean = true;
+  @type("number") invulnTimer: number = 0;
+  @type("uint16") killCount: number = 0;
+  @type("number") score: number = 0;
 }
 
 export class GameStateSchema extends Schema {
-  @type("uint8") version: number = 1;
-  @type({ map: PlayerSchema }) players = new MapSchema<PlayerSchema>();
+  @type("string") phase: string = "playing"; // "playing" | "intermission" | "ended"
+  @type("number") timeRemaining: number = 0;
+  @type("number") intermissionRemaining: number = 0;
+  @type([FactionSchema]) factions = new ArraySchema<FactionSchema>();
+  @type([CharacterSchema]) characters = new ArraySchema<CharacterSchema>();
 }
